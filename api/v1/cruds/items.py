dbac21cdf25e65e71167646c4e01f5f2b8cdf682
岩本圭介 <mcpu3@outlook.jp>
2024-03-20 10:06:13 +0900

    別のグループで同一のバーコードが登録できない問題を修正 #31

--- api/v1/cruds/items.py
+++ api/v1/cruds/items.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from sqlalchemy import or_
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from api.v1 import models, schemas
@@ -9,8 +9,11 @@ from api.v1 import models, schemas
 def read_items(database: Session, group_uuid: str) -> List[models.Item]:
     return database.query(models.Item).filter(models.Item.group_uuid == group_uuid).all()
 
-def read_item(database: Session, item_uuid_or_barcode: str) -> Optional[models.Item]:
-    return database.query(models.Item).filter(or_(models.Item.uuid == item_uuid_or_barcode, models.Item.barcode == item_uuid_or_barcode)).first()
+def read_item(database: Session, item_uuid: str) -> Optional[models.Item]:
+    return database.query(models.Item).filter(models.Item.uuid == item_uuid).first()
+
+def read_item_by_barcode(database: Session, group_uuid: str, barcode: str) -> Optional[models.Item]:
+    return database.query(models.Item).filter(and_(models.Item.group_uuid == group_uuid, models.Item.barcode == barcode)).first()
 
 def create_item(database: Session, group_uuid: str, new_item: schemas.items.NewItem) -> Optional[models.Item]:
     item = models.Item(
